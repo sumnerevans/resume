936c06c1677c1cc7e621666974bb891525f11cf5
Sumner Evans <sumner.evans98@gmail.com>
2016-09-25 10:08:10 -0600

    Fixed some things

--- base/deploy.sh
+++ base/deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+pdflatex resume.tex
+
 echo ""
 echo "========== Deploying Built Website to Server =========="
 scp resume.pdf evansfamilywebsite@the-evans.family:/home/evansfamilywebsite/the-evans.family/sumner/resume.pdf
