9e5094a51a04281d833855c4faebbf6076ccf897
Sumner Evans <sumner.evans98@gmail.com>
2016-11-07 20:42:05 -0700

    Updated RRCC text to have a bullet point

--- base/deploy.sh
+++ base/deploy.sh
@@ -9,8 +9,4 @@ echo "========== Deploying to the-evans.family =========="
 scp resume.pdf tef:/home/evansfamilywebsite/the-evans.family/sumner/
 
 echo ""
-echo "========== Deploying to inside.mines.edu/~jonathanevans =========="
-scp resume.pdf illuminate:/u/eu/an/jonathanevans/public_html
-
-echo ""
 echo "Deploy Successful!"
