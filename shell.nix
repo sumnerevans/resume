473142d25b25c5cec6d63869c87515b1a06f56ae
Sumner Evans <me@sumnerevans.com>
2021-03-14 11:37:57 -0600

    format shell.nix

--- shell.nix
+++ shell.nix
@@ -3,23 +3,21 @@ let
 
   # CoC Config
   cocConfig = pkgs.writeText "coc-settings.json" (
-    builtins.toJSON (
-      {
-        "texlab.path" = "${pkgs.texlab}/bin/texlab";
-      }
-    )
+    builtins.toJSON {
+      "texlab.path" = "${pkgs.texlab}/bin/texlab";
+    }
   );
 in
-  pkgs.mkShell {
-    shellHook = ''
-      mkdir -p .vim
-      ln -sf ${cocConfig} .vim/coc-settings.json
-    '';
+pkgs.mkShell {
+  shellHook = ''
+    mkdir -p .vim
+    ln -sf ${cocConfig} .vim/coc-settings.json
+  '';
 
-    buildInputs = with pkgs; [
-      gnumake
-      rnix-lsp
-      texlab
-      texlive.combined.scheme-full
-    ];
-  }
+  buildInputs = with pkgs; [
+    gnumake
+    rnix-lsp
+    texlab
+    texlive.combined.scheme-full
+  ];
+}
